4e3d25358ca1a46e0d06850809b1230895bd9cfa
Javier Estévez <jestevez@cartolab.es>
2010-10-19 14:51:26 +0200

    New fancy buttons, most of them from ELLE and extDBConnection projects.

--- src/es/udc/cartolab/gvsig/eielutils/misc/EIELValues.java
+++ src/es/udc/cartolab/gvsig/eielutils/misc/EIELValues.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2010. Cartolab (Universidade da Coruña)
- * 
+ *
  * This file is part of extUtilsEIEL
- * 
+ *
  * extUtilsEIEL is free software: you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation, either
  * version 3 of the License, or any later version.
- * 
+ *
  * extUtilsEIEL is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with extUtilsEIEL.
  * If not, see <http://www.gnu.org/licenses/>.
  */
@@ -27,7 +27,7 @@ public class EIELValues {
 
 	public final static Color HEADER_COLOR = new Color(36, 46, 109);
 
-	public final static String FASE = "2008";
+	public final static String FASE = "2010";
 	public final static String PROVINCIA = "36";
 
 	/**
