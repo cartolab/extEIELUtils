57ef8d8f709e3eecb1481e12dbef5945721e7ca2
Francisco Puga <fpuga@cartolab.es>
2011-06-30 19:23:27 +0200

    Minor bugfixing

--- src/es/udc/cartolab/gvsig/eielutils/LoadMapExtension.java
+++ src/es/udc/cartolab/gvsig/eielutils/LoadMapExtension.java
@@ -17,18 +17,11 @@
 
 package es.udc.cartolab.gvsig.eielutils;
 
-import com.iver.andami.PluginServices;
-import com.iver.cit.gvsig.project.documents.view.gui.View;
-
-import es.udc.cartolab.gvsig.eielutils.map.gui.EIELLoadMapWizard;
-
 public class LoadMapExtension extends
 		es.udc.cartolab.gvsig.elle.LoadMapExtension {
 
 	public void execute(String actionCommand) {
-		EIELLoadMapWizard wizard = new EIELLoadMapWizard((View) PluginServices
-				.getMDIManager().getActiveWindow());
-		wizard.open();
+		super.execute(actionCommand);
 	}
 
 	public void initialize() {
