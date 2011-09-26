f1ddf233935286efce5836d2648c6599eff58c71
Francisco Puga <fpuga@cartolab.es>
2011-09-26 15:52:16 +0200

    Allow the creation of the view directly from the elle load map button

--- src/es/udc/cartolab/gvsig/eielutils/LoadMapExtension.java
+++ src/es/udc/cartolab/gvsig/eielutils/LoadMapExtension.java
@@ -17,20 +17,23 @@
 
 package es.udc.cartolab.gvsig.eielutils;
 
-import com.iver.andami.PluginServices;
 import com.iver.cit.gvsig.project.documents.view.gui.View;
 
 import es.udc.cartolab.gvsig.eielutils.map.gui.EIELLoadMapWizard;
 
+
 public class LoadMapExtension extends
 		es.udc.cartolab.gvsig.elle.LoadMapExtension {
 
+	@Override
 	public void execute(String actionCommand) {
-		EIELLoadMapWizard wizard = new EIELLoadMapWizard((View) PluginServices
-				.getMDIManager().getActiveWindow());
+
+	View view = createViewIfNeeded();
+	EIELLoadMapWizard wizard = new EIELLoadMapWizard(view);
 		wizard.open();
 	}
 
+	@Override
 	public void initialize() {
 
 	}
